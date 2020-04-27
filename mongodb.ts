ffdf871082d52c9826d0afd392764273d579f4d5
Evan <ehutch7@gmail.com>
2020-04-27 14:08:22 -0400

    Making our mongodb file typescript. Adding and testing some db stuff to make sure everything is working correctly -- it is.

--- mongodb.ts
+++ mongodb.ts
@@ -1,6 +1,4 @@
-const mongodb = require("mongodb");
-
-const MongoClient = mongodb.MongoClient;
+import { MongoError, MongoClient } from "mongodb";
 
 const CONNECTION_URL = "mongodb://127.0.0.1:27017";
 const DATABASE_NAME = "task-manager";
@@ -10,10 +8,17 @@ MongoClient.connect(
     {
         useNewUrlParser: true,
     },
-    (error, client) => {
+    (error: MongoError, client: MongoClient) => {
         if (error) {
             return console.log("UNABLE TO CONNECT TO DB: \n", error);
         }
         console.log("~~CONNECTED TO DB CLIENT~~");
+
+        const db = client.db(DATABASE_NAME);
+
+        db.collection("users").insertOne({
+            age: 29,
+            name: "Evan",
+        });
     }
 );
