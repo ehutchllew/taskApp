4d5fe1e0b295c9b36ae8bf0c36d75c85da0dd5ea
Evan <ehutch7@gmail.com>
2020-05-16 21:25:14 -0400

    Updating tasks route handlers to conform to the async/await and errorHandler flow.

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,36 +1,42 @@
 import { Application } from "express";
+import { errorHandler } from "../common/errorHandler";
 import { Task } from "../db/documents";
+import { IError, SERVICE_ERRORS } from "../types/errors";
 
 export function taskRoutes(app: Application) {
-    app.get("/tasks", (req, res) => {
-        Task.find({})
-            .then((resp) => {
-                res.send(resp);
-            })
-            .catch((e) => {
-                res.status(500).send(e);
-            });
+    app.get("/tasks", async (req, res) => {
+        try {
+            const tasks = await Task.find({});
+            res.send(tasks);
+        } catch (e) {
+            const err: IError = errorHandler(e);
+            res.status(err.status).send(err);
+        }
     });
 
-    app.get("/tasks/:id", (req, res) => {
-        Task.findById(req.params.id)
-            .then((resp) => {
-                res.send(resp);
-            })
-            .catch((e) => {
-                res.status(404).send(e);
-            });
+    app.get("/tasks/:id", async (req, res) => {
+        try {
+            const task = Task.findById(req.params.id);
+
+            if (!task) {
+                throw { name: SERVICE_ERRORS.DOCUMENT_NOT_FOUND };
+            }
+
+            res.send(task);
+        } catch (e) {
+            const err: IError = errorHandler(e);
+            res.status(err.status).send(err);
+        }
     });
 
-    app.post("/tasks", (req, res) => {
-        const task = new Task(req.body);
-
-        task.save()
-            .then(() => {
-                res.status(201).send(task);
-            })
-            .catch((e) => {
-                res.status(400).send(e);
-            });
+    app.post("/tasks", async (req, res) => {
+        try {
+            const task = new Task(req.body);
+            await task.save();
+            res.status(201).send(task);
+        } catch (e) {
+            const err: IError = errorHandler(e);
+            res.status(err.status).send(err);
+        }
     });
 }
