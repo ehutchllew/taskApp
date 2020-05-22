840404bd9aac8bfcde28b91450b67d04beb12c55
Evan <ehutch7@gmail.com>
2020-05-22 15:57:34 -0400

    Adjusting a yarn script and adding timestamps to our schemas.

--- src/db/schemas.ts
+++ src/db/schemas.ts
@@ -10,76 +10,86 @@ import { Task, User } from "./models";
 export type ITaskDocument = Document & ITaskField;
 export type IUserDocument = Document & IUserField;
 
-const TaskSchema = new Schema({
-    completed: {
-        default: false,
-        type: Boolean,
+const TaskSchema = new Schema(
+    {
+        completed: {
+            default: false,
+            type: Boolean,
+        },
+        description: {
+            required: true,
+            trim: true,
+            type: String,
+        },
+        owner: {
+            ref: "user",
+            required: true,
+            type: Schema.Types.ObjectId,
+        },
     },
-    description: {
-        required: true,
-        trim: true,
-        type: String,
-    },
-    owner: {
-        ref: "user",
-        required: true,
-        type: Schema.Types.ObjectId,
-    },
-});
+    {
+        timestamps: true,
+    }
+);
 
-const UserSchema = new Schema({
-    age: {
-        type: Number,
-        validate(value) {
-            if (value < 0) {
-                throw new Error("Age must be above 0");
-            }
-            return true;
-        },
-    },
-    email: {
-        lowercase: true,
-        required: true,
-        trim: true,
-        type: String,
-        unique: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error("Invalid e-mail");
-            }
-            return true;
-        },
-    },
-    name: {
-        required: true,
-        trim: true,
-        type: String,
-    },
-    password: {
-        minlength: 7,
-        required: true,
-        trim: true,
-        type: String,
-        validate(value) {
-            if (value.includes("password")) {
-                throw new Error("Password cannot contain 'password'");
-            }
-            return true;
-        },
-    },
-    role: {
-        default: ROLE.USER,
-        type: String,
-    },
-    tokens: [
-        {
-            token: {
-                required: true,
-                type: String,
+const UserSchema = new Schema(
+    {
+        age: {
+            type: Number,
+            validate(value) {
+                if (value < 0) {
+                    throw new Error("Age must be above 0");
+                }
+                return true;
             },
         },
-    ],
-});
+        email: {
+            lowercase: true,
+            required: true,
+            trim: true,
+            type: String,
+            unique: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error("Invalid e-mail");
+                }
+                return true;
+            },
+        },
+        name: {
+            required: true,
+            trim: true,
+            type: String,
+        },
+        password: {
+            minlength: 7,
+            required: true,
+            trim: true,
+            type: String,
+            validate(value) {
+                if (value.includes("password")) {
+                    throw new Error("Password cannot contain 'password'");
+                }
+                return true;
+            },
+        },
+        role: {
+            default: ROLE.USER,
+            type: String,
+        },
+        tokens: [
+            {
+                token: {
+                    required: true,
+                    type: String,
+                },
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
 
 UserSchema.virtual("tasks", {
     localField: "_id",
