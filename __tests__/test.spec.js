b6db16e1729221ca1c32f60c1ea5582160f0736a
Evan <ehutch7@gmail.com>
2020-06-02 15:44:04 -0400

    Adding environment variables to a jest config file. Tested it and it is logging them correctly.

---
+++ __tests__/test.spec.js
@@ -0,0 +1,7 @@
+describe("Test suite - testing app service", () => {
+    beforeAll(() => {
+        console.log("coming inside beforeAll", process.env);
+    });
+
+    it("tests something", () => {});
+});
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+process.env.DB_HOST_URL = "mongodb://127.0.0.1:27017/task-manager-api-test";
+process.env.JWT_SECRET_KEY = "somesecretlel";
+process.env.PORT = 3001;
